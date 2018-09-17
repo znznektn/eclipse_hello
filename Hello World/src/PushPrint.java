7e56049ed509474d9ad93d6911491ad4ae3b9a79
znznektn <43328113+znznektn@users.noreply.github.com>
2018-09-17 17:25:17 +0900

    Update PushPrint.java
    GitHub for git pull

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,6 +4,7 @@ public class PushPrint {
 		System.out.println("Add Java Class File Push");
 		System.out.println("World with Miku");	
 		System.out.println("World with 42");	
+		System.out.println("World with Hatsune");	
 
 	}
 }
